105d7ffbdcf87f773af7b9f2b4b215b20dd8d0aa
Guilherme Nogueira <nogueira.gfn@gmail.com>
2026-05-11 10:32:00 -0300

    scaffold zig project

---
+++ build.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const mod = b.addModule("avl", .{
+        .root_source_file = b.path("src/avl.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const tests = b.addTest(.{ .root_module = mod });
+    const run_tests = b.addRunArtifact(tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_tests.step);
+}
